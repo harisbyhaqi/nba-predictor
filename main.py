e2f7ef97502efa028899d62ee1d2c4b95389b0ab
Haris Byhaqi <harisbyhaqi11@gmail.com>
2025-09-16 15:39:27 -0400

    resolved

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from nba_api.stats.endpoints import leaguedashplayerstats
+# src/data_download.py
+import pandas as pd
+from nba_api.stats.endpoints import leaguegamefinder
+from tqdm import tqdm
 
+def download_seasons(seasons):
+    frames = []
+    for season in seasons:
+        print(f"Fetching season {season}")
+        lg = leaguegamefinder.LeagueGameFinder(season_nullable=season)
+        df = lg.get_data_frames()[0]
+        frames.append(df)
+    out = pd.concat(frames, ignore_index=True)
+    return out
 
-
-def main():
-
-    print("hello")
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    seasons = ["2024-25", "2023-24"]
+    df = download_seasons(seasons)
+    df.to_csv("data/raw/games_raw.csv", index=False)
+    print("Saved to data/raw/games_raw.csv")
